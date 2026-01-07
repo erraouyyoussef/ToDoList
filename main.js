12d74e371df86927e134786aee7ad211a2153601
YOUSSEF DEV <yousseferaoui@gmail.com>
2026-01-07 21:22:59 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -3,17 +3,28 @@ let inputfiled = document.querySelector('#inputfiled');
 let btn = document.querySelector('#btn');
 let ul = document.querySelector('#ul');
 
+
 btn.addEventListener('click', function () {
     if (inputfiled.value.length > 0) {
         let li = document.createElement('li');
-        li.textContent = inputfiled.value.trim();
-        ul.appendChild(li);
+        li.textContent = inputfiled.value;
+        ul.append(li);
         inputfiled.value = '';
+        saveData();
 
     } else {
         alert("Type You Task First")
     };
-
 });
+
+
+function saveData() {
+    localStorage.setItem('data', ul.innerHTML);
+}
+function showData() {
+    ul.innerHTML = localStorage.getItem("data");
+}
+showData()
+
 
 
