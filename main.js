e97002f466e2a1897c7a044cab9c3454d296d425
YOUSSEF DEV <yousseferaoui@gmail.com>
2026-01-09 02:07:01 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,49 +1,3 @@
-// let container = document.querySelector('.container');
-// let inputfiled = document.querySelector('#inputfiled');
-// let btn = document.querySelector('#btn');
-// let ul = document.querySelector('#ul');
-
-
-
-
-
-// function addTask() {
-//     if (inputfiled.value.length > 0) {
-//         let list = document.createElement('li');
-//         let span = document.createElement('span');
-//         list.textContent = inputfiled.value;
-//         ul.append(list);
-//         span.textContent = ('⛔');
-//         list.append(span);
-//         ul.append(list);
-//         inputfiled.value = '';
-
-//         span.onclick = function () {
-//             list.remove();
-//             saveData()
-//         };
-//         list.onclick = function () {
-//             list.classList.toggle('checked');
-//             saveData()
-//         }
-
-//     } else {
-//         alert("Type You Task First")
-//     };
-//     saveData()
-// }
-
-// btn.addEventListener('click', addTask);
-
-
-// function saveData() {
-//     localStorage.setItem('data', ul.innerHTML);
-// }
-// function showData() {
-//     ul.innerHTML = localStorage.getItem("data");
-// }
-// showData()
-// localStorage.clear()
 
 let inputfiled = document.querySelector('#inputfiled');
 let btn = document.querySelector('#btn');
@@ -56,7 +10,7 @@ function addTask() {
         alert("Type your task first");
     }
     else if (task.length > 30) {
-        alert("Task should be less than 30 letters");
+        alert("Task should be less than 10 letters");
     }
     else {
         let li = document.createElement('li');
@@ -99,7 +53,6 @@ function showData() {
     ul.innerHTML = localStorage.getItem('data') || '';
 }
 showData();
-
 
 
 
